4c8d7b8ca902473322b8b5ef59057a4bb91a22b5
Nikita Ponomarenko <nikita@teknikacorp.com>
2024-05-12 02:31:19 +0300

    - Init;

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache")
+    .WithImage("ghcr.io/microsoft/garnet")
+    .WithImageTag("latest");
+
+var db = builder.AddPostgres("snowcoreBlog.Db");
+
+builder.AddProject<Projects.snowcoreBlog_Backend_AdminsManagement>("backend_adminsmanagement")
+    .WithReference(cache)
+    .WithReference(db);
+
+builder.AddProject<Projects.snowcoreBlog_Backend_UsersManagement>("backend_usersmanagement")
+    .WithReference(cache)
+    .WithReference(db);
+
+builder.AddProject<Projects.snowcoreBlog_Backend_Articles>("backend_articles")
+    .WithReference(cache)
+    .WithReference(db);
+
 await builder.Build().RunAsync();
